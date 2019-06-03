d85742bc95490cf0f8367901e034791924dd392d
AlaCrity <wlchen1105@gmail.com>
2019-06-03 00:36:15 -0400

    made changes to the javascript file and added CSS styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var player;
 var defender;
 var charArray = [];
 var playerSelected = false;
-var defenseSelected = false;
+var defenderSelected = false;
 
 function Character(name, hp, ap, counter, pic) {
   this.name = name;
@@ -44,23 +44,26 @@ function initilizeCharacters() {
   );
   charArray.push(harry, draco, bellatrix, voldemort);
 }
-
-function characterCards(divID) {
-  $("divID")
+function populateCharacterCards(divID) {
+  const myCardContainer = $("#" + divID);
+  //   console.warn("charArray", charArray);
+  $(myCardContainer)
     .children()
     .remove();
   for (var i = 0; i < charArray.length; i++) {
-    $(divID).append("<div />");
-    $(divID + " div:last-child").addClass("card");
-    $(divID + " div:last-child").append("<img />");
-    $(divID + " img:last-child").attr("id", charArray[i].name);
-    $(divID + " img:last-child").attr("class", "card-img-top");
-    $(divID + " img:last-child").attr("src", charArray[i].pic);
-    $(divID + " img:last-child").attr("width", 150);
-    $(divID + " img:last-child").addClass("img-thumbnail");
-    $(divID + " div:last-child").append(charArray[i].name + "<br>");
-    $(divID + " div:last-child").append("HP: " + charArray[i].healthPoints);
-    $(divID + " div:last-child").append();
+    // console.log(i, "append: ", myCardContainer);
+    const card = document.createElement("div");
+    const cardImage = document.createElement("img");
+    $(card).addClass("card");
+    $(card).append(cardImage);
+    $(card).attr("id", charArray[i].name);
+    $(cardImage).attr("class", "card-img-top");
+    $(cardImage).attr("src", charArray[i].pic);
+    $(cardImage).attr("width", 150);
+    $(cardImage).addClass("img-thumbnail");
+    $(card).append(charArray[i].name + "<br>");
+    $(card).append("HP: " + charArray[i].healthPoints);
+    $(myCardContainer).append(card);
   }
 }
 
@@ -86,38 +89,67 @@ Character.prototype.counterAttack = function(Obj) {
       " damage points."
   );
 };
+function setBaseAttack(Obj) {
+  baseAttack = Obj.attackPower;
+}
+function isAlive(Obj) {
+  if (Obj.healthPoints > 0) {
+    return true;
+  }
+  return false;
+}
 
-// let divTag1 = $("<div>");
+$(document).on("click", "img", function() {
+  if (playerSelected && !defenderSelected && this.id != player.name) {
+    for (var i = 0; i < charArray.length; i++) {
+      if (charArray[i].name == this.id) {
+        defender = charArray[i];
+        charArray.splice(i, 1);
+        defenderSelected = true;
+        $("#msg").html("Click on the button to cast a spell and attack!");
+      }
+    }
+    $("#defenderDiv").append(this);
+    $("#defenderDiv").append("<br>" + defender.name);
+    $("#defenderHealthDiv").append("HP: " + defender.healthPoints);
+  }
 
-// divTag1.attr("class", "card");
-// divTag1.css("width", "18rem");
+  $(document).on("click", "#attackBtn", function() {
+    if (playerSelected && defenderSelected) {
+      if (isAlive(player) && isAlive(defender)) {
+        player.attack(defender);
+        defender.counterAttack(player);
+        $("#playerHealthDiv").html("HP: " + player.healthPoints);
+        $("#defenderHealthDiv").html("HP: " + defender.healthPoints);
+        if (!isAlive(defender)) {
+          $("#defenderHealthDiv").html("Defeated!");
+          $("#playerHealthDiv").html("Opponent Defeated!");
+          $("#msg").html("Pick another opponent to duel");
+        }
+        if (!isAlive(player)) {
+          $("#playerHealthDiv").html("You have been defeated");
+          $("#msg").html("Do you want to try again?");
+          $("#attackBtn").html("Restart Duel");
+          $(document).on("click", "#attackBtn", function() {
+            location.reload();
+          });
+        }
+      }
+    }
+  });
+  if (!playerSelected) {
+    for (var i = 0; i < charArray.length; i++) {
+      if (charArray[i].name == this.id) {
+        player = charArray[i];
+        setBaseAttack(player);
+        charArray.splice(i, 1);
+        playerSelected = true;
+      }
+    }
+  }
+});
 
-// let divTag2 = $("<div>");
-// divTag1.attr("class", "card-body");
-
-// let hTag = $("<h2>");
-// hTag.attr("class", "card-body");
-// hTag.text("Char Name");
-
-// let pTag = $("<p>");
-// pTag.attr("class", "card-text");
-// pTag.text("some text here");
-
-// divTag2.append(hTag, pTag);
-// divTag1.append(divTag2);
-
-// $("#cards").append(divTag1);
-
-// {
-//   <div class="card" style="width: 18rem;">
-//       <div class="card-body">
-//         <h2 class="card-title">Card title</h2>
-//         <img src="https://via.placeholder.com/155x155"></img>
-//         <h3 class="card-subtitle mb-2 text-muted">Card subtitle</h3>
-//         <p class="card-text">
-//           Some quick example text to build on the card title and make up the
-//           bulk of the card's content.
-//         </p>
-//       </div>
-//     </div>
-// }
+$(document).ready(function() {
+  initilizeCharacters();
+  populateCharacterCards("cards");
+});
